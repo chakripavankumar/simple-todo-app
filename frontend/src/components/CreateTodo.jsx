a677fc84fee6fe731d7bfbcf2d20435aa76f71be
chakripavankumar <siriyalapavankumar@gmail.com>
2024-01-17 16:06:17 +0530

    almost✅

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react"
+
 export function CreateTodo(){
+    const [tittle,setTittle]=useState("");
+    const [description,setDescription]= useState("");
     return (
         <div>
-    <input type="text" placeholder="title"></input>  <br/>
-    <input type="text" placeholder="description"></input>  <br/>
+    <input type="text" placeholder="title" onChange={ function(e){
+         const value = e.target.value;
+         setTittle(e.target.value);
+    } }></input>  <br/>
+    <input type="text" placeholder="description" onChange={function(e) {
+            const value = e.target.value;
+            setDescription(e.target.value)
+    }}></input>  <br/>
 
-    <button> Add a todo</button>
+    <button onClick={()=>{
+         fetch("http://localhost:3000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+                title: title,
+                description: description
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+            .then(async function(res) {
+                const json = await res.json();
+                alert("Todo added");
+            })
+    }}> Add a todo</button>
         </div>
     )
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+To do App
+This project contains a simple todo application
+It has the following features-
+-> A user can create a todo
+-> A user can see their existing todo’s
+-> A user can mark todo is done✅
